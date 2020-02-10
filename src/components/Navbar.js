b1b16122a56ee54df1af6f1932f0a70d64244e2f
mateuszhadrian <tenhadrian@gmail.com>
2020-02-10 22:11:22 +0100

    dropdown  to navbar added when logged in/ logout option added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { logoutUser, getUserData } from '../redux/actions/userActions'
 
 class Navbar extends Component {
+    state = {
+        show: false
+    }
+showOptions = () => {
+    return this.setState({
+        show: !this.state.show
+    })
+}
+
+logout = () => {
+    this.props.logoutUser()
+    this.setState({
+        show: false
+    })
+}
+
     render() {
 
         const { user: { authenticated, loading, credentials : { imageUrl, handle }}} = this.props
 
         const loggedUserPhoto = {
-            marginTop: '8px',
+            marginTop: '0',
             backgroundImage: `url(${imageUrl})`,
             height: '50px',
             width: '50px',
@@ -18,12 +35,25 @@ class Navbar extends Component {
             backgroundPosition: 'center'
         }
 
+        const optionsDropdown = this.state.show && (
+            <div className="options__container">
+                <div className="options">
+                    <ul className="options__items">
+                        <li className="options__item"><button className="options__button">PROFIL</button></li>
+                        <li className="options__item"><button onClick={this.logout} className="options__button"><span className="logout">WYLOGUJ</span></button></li>
+                    </ul>
+                </div>
+            </div>
+        )
+
         const navbarRight = !loading ? (
             authenticated ? (
-                <div className="navbar__profile">
-                    <div style={loggedUserPhoto}></div>
-                    <span className="profile__username">{handle}</span>
+                <button onClick={this.showOptions} className="profile__button">
+                    <div className="navbar__profile">
+                        <div className="profile__picture" style={loggedUserPhoto}></div>
+                        <span className="profile__username">{handle}</span>
                 </div>
+                </button>
             ) : (
                 <div className="navbar__buttons-area">
                         <Link className="navbar__button navbar__button--signup" to="/rejestracja"><span >rejestracja</span></Link> 
@@ -35,10 +65,13 @@ class Navbar extends Component {
         )
         return (
             <div className='navbar'>
-                <div className='navbar__container'>
-                    <Link className='navbar__brand' to="/"><span >SaySome</span></Link>
-                    {navbarRight}
-                </div>
+                <div className="profile__options-container">
+                    <div className='navbar__container'>
+                        <Link className='navbar__brand' to="/"><span >SaySome</span></Link>
+                        {navbarRight}
+                    </div>
+                    {optionsDropdown}
+                </div> 
             </div>
         )
     }
@@ -48,6 +81,8 @@ const mapStateToProps = (state) => ({
     user: state.user
 })
 
-const mapActionsToProps = null;
+const mapActionsToProps = {
+    logoutUser
+};
 
 export default connect(mapStateToProps, mapActionsToProps)(Navbar)
