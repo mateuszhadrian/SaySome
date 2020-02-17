6faab7d94cad4a5beba908bc29a10c7de1ad413d
mateuszhadrian <tenhadrian@gmail.com>
2020-02-18 00:37:19 +0100

    Updates

---
+++ precache-manifest.10a10f62f7a8f711ab3aaa4047acd01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243e590fcd207038b7a68e6d85d5131b",
+    "url": "/SaySome/index.html"
+  },
+  {
+    "revision": "7a6a72e90db2ca1ecdd8",
+    "url": "/SaySome/static/css/main.9a502aef.chunk.css"
+  },
+  {
+    "revision": "938b33aac7c5470e1250",
+    "url": "/SaySome/static/js/2.e9b828a1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/SaySome/static/js/2.e9b828a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6a72e90db2ca1ecdd8",
+    "url": "/SaySome/static/js/main.4bfdd348.chunk.js"
+  },
+  {
+    "revision": "a38b5c76a8f4e9b7b05f",
+    "url": "/SaySome/static/js/runtime-main.d1b9e585.js"
+  }
+]);
